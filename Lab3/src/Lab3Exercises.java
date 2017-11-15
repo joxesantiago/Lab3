47624ba6e471de73431e8f4e1cff18f784fe8c26
4015e18 <4015e18@guineo.ece.uprm.edu>
2017-11-15 10:37:27 -0400

    New Title Uncommented second line to setDefaultCloseOperation New Frame Location

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,10 +2,10 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		JFrame myFrame = new JFrame("Jose A. Santiago Martinez");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setLocation(825, 440);
+		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 	}
 }
