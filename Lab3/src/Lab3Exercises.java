8c6131c11b159c80b85530f82b222c3479988aae
4015e18 <4015e18@guineo.ece.uprm.edu>
2017-11-15 10:40:51 -0400

    Changed Location

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -4,7 +4,7 @@ public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Jose A. Santiago Martinez");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(825, 440);
+		myFrame.setLocation(825, 430);
 		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 	}
